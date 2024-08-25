b061700cae1000524c902ad23cfc7a42fdaca058
Jesus Trejo <jesustrejo10@gmail.com>
2024-08-24 21:12:04 -0400

    Adding HomeView, ListView and ListViewModel

---
+++ iosApp/iosApp/Preview Content/ListView.swift
@@ -0,0 +1,19 @@
+//
+//  ListView.swift
+//  iosApp
+//
+//  Created by Camila Juliao on 24/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
---
+++ iosApp/iosApp/Preview Content/ListViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewModel.swift
+//  iosApp
+//
+//  Created by Camila Juliao on 24/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListViewModel()
+}
