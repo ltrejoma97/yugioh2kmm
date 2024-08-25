9d1688b08e3cd0572052262afdf34dd33dd70e03
Jesus Trejo <jesustrejo10@gmail.com>
2024-08-24 22:02:24 -0400

    Adding MapView, WorldView and Fixing others views

---
+++ iosApp/iosApp/Preview Content/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  iosApp
+//
+//  Created by Camila Juliao on 24/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
---
+++ iosApp/iosApp/Preview Content/WorldView.swift
@@ -0,0 +1,19 @@
+//
+//  WorldView.swift
+//  iosApp
+//
+//  Created by Camila Juliao on 24/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct WorldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorldView()
+}
