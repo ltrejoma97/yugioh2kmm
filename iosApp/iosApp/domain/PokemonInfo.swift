8e95120102664b4cfd3258add4d2a4b257fc8df4
Jesus Trejo <jesustrejo10@gmail.com>
2024-08-31 23:00:04 -0400

    Adding yugioh api call

---
+++ iosApp/iosApp/domain/PokemonInfo.swift
@@ -0,0 +1,19 @@
+//
+//  PokemonInfo.swift
+//  iosApp
+//
+//  Created by Camila Juliao on 26/8/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct PokemonInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonInfo()
+}
