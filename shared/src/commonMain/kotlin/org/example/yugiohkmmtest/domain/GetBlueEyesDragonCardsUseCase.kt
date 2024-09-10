944a8be0a8811068acacce7312e597e031f502e7
luisT <luistrejo94@gmail.com>
2024-09-09 21:47:32 -0400

    fixing return of use case

--- shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/GetBlueEyesDragonCardsUseCase.kt
+++ shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/GetBlueEyesDragonCardsUseCase.kt
@@ -1,15 +1,15 @@
 package org.example.yugiohkmmtest.domain
 
 import org.example.yugiohkmmtest.data.Endpoint
-
+import org.example.yugiohkmmtest.domain.DTO.CardDTOResponse
 
 
 interface GetBlueEyesDragonCardsUseCase {
-    suspend fun invoke(): String
+    suspend fun invoke(): CardDTOResponse
 }
 
 class GetBlueEyesDragonCardsUseCaseImp(val endpoint: Endpoint) : GetBlueEyesDragonCardsUseCase{
-    override suspend fun invoke(): String {
+    override suspend fun invoke(): CardDTOResponse {
         return endpoint.getblueDragon()
     }
 }
