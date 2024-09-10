ba930da328ea05bc44cd4b312240cdc6adaf1997
Jesus Trejo <jesustrejo10@gmail.com>
2024-09-10 15:36:00 -0400

    fix test 5

--- iosApp/iosApp/presentation/GetBlueEyesDragonCardsViewModel.swift
+++ iosApp/iosApp/presentation/GetBlueEyesDragonCardsViewModel.swift
@@ -12,14 +12,14 @@ import Shared
 extension GetBlueEyesDragonCardsScreen{
     
     @MainActor class GetBlueEyesDragonCardsViewModel: ObservableObject{
-//        private let getBlueEyesDragonCardsUseCaseHelper = GetBlueEyesDragonCardsUseCaseHelper.init()
+        private let getBlueEyesDragonCardsUseCaseHelper = GetBlueEyesDragonCardsUseCaseHelper.init()
         private let getBlueEyesDragonCardsUseCase = GetBlueEyesDragonCardsUseCaseImp(endpoint: EndpointImp())
         @Published private(set) var blueEyesDragons = ""
         
         func getBlueEyesDragons() async{ 
             do{
                 print("Hola1")
-                let getBlueEyesDragons1 = try await getBlueEyesDragonCardsUseCase.invoke()
+                let getBlueEyesDragons1 = try await getBlueEyesDragonCardsUseCaseHelper.callUseCase()
                 print("Hola2")
                 self.blueEyesDragons = self.blueEyesDragons + getBlueEyesDragons1
                 print("Hola3")
