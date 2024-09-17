c1ad90b6b48a562146aa25dedd5aa7e9a3fe799e
luisT <luistrejo94@gmail.com>
2024-09-17 14:47:49 -0400

    comment databaseCard

--- shared/src/commonMain/kotlin/org/example/yugiohkmmtest/data/LocalDataSource/DatabaseCard.kt
+++ shared/src/commonMain/kotlin/org/example/yugiohkmmtest/data/LocalDataSource/DatabaseCard.kt
@@ -5,32 +5,32 @@ import org.example.yugiohkmmtest.data.TypeOfCard
 import org.example.yugiohkmmtest.domain.DTO.CardImages
 import org.example.yugiohkmmtest.domain.DTO.CardPrices
 
-class DatabaseCard(
-     var id :Int,
-     val name : Int,
-     val type : String,
-     val desc :String,
-     val race : String = "",
-     val ygoprodeck_url : String,
-     val archetype : String,
-     val humanReadableCardType : String,
-     val atk : Int =0 ,
-     val def : Int = 0,
-     val frameType: String,
-     val level : Int = 0,
-     val attribute : String = "",
-     val card_images: List<CardImages>,
-     val card_prices: List<CardPrices>): RealmObject {
-
-//     fun getTypeOfCard(): TypeOfCard {
-//          when (this.humanReadableCardType){
+//class DatabaseCard(
+//     var id :Int,
+//     val name : Int,
+//     val type : String,
+//     val desc :String,
+//     val race : String = "",
+//     val ygoprodeck_url : String,
+//     val archetype : String,
+//     val humanReadableCardType : String,
+//     val atk : Int =0 ,
+//     val def : Int = 0,
+//     val frameType: String,
+//     val level : Int = 0,
+//     val attribute : String = "",
+//     val card_images: List<CardImages>,
+//     val card_prices: List<CardPrices>): RealmObject {
 //
-//               return TypeOfCard.Spell
+////     fun getTypeOfCard(): TypeOfCard {
+////          when (this.humanReadableCardType){
+////
+////               return TypeOfCard.Spell
+////
+////          }
+////     }
 //
-//          }
-//     }
-
-}
+//}
 const val SPELL_CARD = "spell"
 const val MONSTER_CARD = "monster"
 const val TRAP_CARD = "trao"
