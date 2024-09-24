03d7d4383aa3cf4d7a32895a381233a3172232cb
luisT <luistrejo94@gmail.com>
2024-09-24 15:05:37 -0400

    fixing base use case response

--- shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/GetBlueEyesDragonCardsUseCase.kt
+++ shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/GetBlueEyesDragonCardsUseCase.kt
@@ -5,22 +5,24 @@ package org.example.yugiohkmmtest.domain
 import org.example.yugiohkmmtest.data.repository.CardsRepository
 import org.example.yugiohkmmtest.domain.modelObjexts.YugiohCard
 import org.example.yugiohkmmtest.domain.modelObjexts.mapToUiListCard
-import org.example.yugiohkmmtest.domain.useCases.BaseUseCaseResponse
+import org.example.yugiohkmmtest.domain.useCases.BaseResponse
+import org.example.yugiohkmmtest.domain.useCases.GeneralError
+import org.example.yugiohkmmtest.domain.useCases.UseCaseResponse
 
 
 interface GetBlueEyesDragonCardsUseCase {
-    @Throws(Exception::class) suspend fun invoke(): BaseUseCaseResponse<List<YugiohCard>>
+    @Throws(Exception::class) suspend fun invoke(): UseCaseResponse<List<YugiohCard>>
     fun testChannel() : String
 }
 
 class GetBlueEyesDragonCardsUseCaseImp(val repository: CardsRepository) : GetBlueEyesDragonCardsUseCase{
-    override suspend fun invoke(): BaseUseCaseResponse<List<YugiohCard>> {
+    override suspend fun invoke(): UseCaseResponse<List<YugiohCard>> {
         try {
             val response =  repository.getBlueEyesCards()
             val cardList : List<YugiohCard> = response.mapToUiListCard()
-            return BaseUseCaseResponse.Success(cardList)
+            return UseCaseResponse.success(cardList)
         }catch (e:Exception){
-            return BaseUseCaseResponse.Error(e.message.toString())
+            return UseCaseResponse.error(GeneralError.KnownError(e.message.toString()))
         }
     }
 
--- shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/useCases/BaseUseCase.kt
+++ shared/src/commonMain/kotlin/org/example/yugiohkmmtest/domain/useCases/BaseUseCase.kt
@@ -1,23 +1,58 @@
 package org.example.yugiohkmmtest.domain.useCases
 
-// Clase abstracta BaseUseCase
-abstract class BaseUseCase<in Params, Result> {
+data class BaseResponse(
+    val data : ResponseData?,
+    val success : Boolean = true
+){
+    inline fun <reified T> asData() : T? {
+        if( data is T)
+            return data
+        return null
+    }
+    companion object{
+        fun <T> success(data : T): BaseResponse {
+            return BaseResponse(ResponseData.SuccessData(data))
+        }
 
-
-
-    // Manejo de errores
-    protected fun handleError(error: String): String {
-        // Aquí puedes implementar la lógica para manejar el error
-        // Por ejemplo, podrías lanzar una excepción o devolver un resultado específico
-        return  error
-//        throw Exception(error)
+        fun error(generalError: GeneralError): BaseResponse {
+            return BaseResponse(ResponseData.ErrorData(generalError),false)
+        }
     }
 }
 
-// Clase para manejar respuestas y errores en los casos de uso
-sealed class BaseUseCaseResponse<out T> {
-    data class Success<out T>(val data: T) : BaseUseCaseResponse<T>()
-    data class Error(val message: String) : BaseUseCaseResponse<Nothing>()
+sealed class ResponseData {
+
+    data class SuccessData<T>(val data : T) : ResponseData()
+
+    data class ErrorData(val generalError : GeneralError = GeneralError.UnknownError()) : ResponseData()
+
+}
 
 
+sealed class GeneralError {
+
+    data class UnknownError(val errorCause : GeneralErrorCause = GeneralErrorCause.UNKNOWN) : GeneralError()
+    data class KnownError(val error :String) : GeneralError()
+    data class ResourceError(val error :Int) : GeneralError()
+}
+
+enum class GeneralErrorCause  {
+    NO_NETWORK, BAD_REQUEST, UNAUTHORIZED , UNKNOWN, WRONG_FORM
+}
+
+
+class UseCaseResponse<T>(
+    val success : Boolean = true,
+    val data : T? = null,
+    val error : GeneralError? = null
+) {
+    companion object{
+        fun <T> success(data : T): UseCaseResponse<T> {
+            return UseCaseResponse(data = data)
+        }
+
+        fun <T> error(generalError : GeneralError?): UseCaseResponse<T> {
+            return UseCaseResponse(success = false,data = null,error = generalError)
+        }
+    }
 }
